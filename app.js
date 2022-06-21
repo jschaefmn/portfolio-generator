f02db0afa46e45a955172cf3fac52f9ef49ed3b4
Jake Schaefbauer <jschaefmn@gmail.com>
2022-06-20 21:59:35 -0500

    almost done with 9.1.6

---
+++ app.js
@@ -0,0 +1,22 @@
+// const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+// };
+
+// printProfileData(profileDataArgs);
+
+const message = 'Hello Node!';
+
+if (true === true) {
+  const message = 'Hello ES6!';
+  let sum = 5;
+  sum += 10;
+  console.log(message);
+  console.log(sum);
+}
+
+console.log(message);
+console.log(sum);
