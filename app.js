59eed8efc025b7251cf76e1dedbf04f0d314c9ae
Jake Laptop <jschaefmn@gmail.com>
2022-06-21 16:13:33 -0500

    finished 9.1.6

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-// const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2);
 
-// const printProfileData = profileDataArr => {
-//   for (let i = 0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//   }
-// };
+const printProfileData = profileDataArr => {
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
 
-// printProfileData(profileDataArgs);
-
-const message = 'Hello Node!';
-
-if (true === true) {
-  const message = 'Hello ES6!';
-  let sum = 5;
-  sum += 10;
-  console.log(message);
-  console.log(sum);
-}
-
-console.log(message);
-console.log(sum);
+printProfileData(profileDataArgs);
