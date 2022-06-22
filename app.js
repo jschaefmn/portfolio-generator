ae50e99e3e293c6fd5e1e12724bb22024e0f2602
Jake Laptop <jschaefmn@gmail.com>
2022-06-22 17:30:08 -0500

    halfway done with 9.3.5

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template')
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
