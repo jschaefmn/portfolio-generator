77e479a0e935e46a40cc8933d75ef1042219411b
Jake Laptop <jschaefmn@gmail.com>
2022-06-22 15:57:25 -0500

    finished lesson 2

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 const fs = require('fs');
+const generatePage = require('./src/page-template')
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+const pageHTML = generatePage(name, github);
 
-  <body>
-  <h1>${name}</h1>
-  <h2><a href="https://github.com/${github}">GitHub</a></h2>
-  </body>
-  </html>
-  `;
-};
-
-fs.writeFile('index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
